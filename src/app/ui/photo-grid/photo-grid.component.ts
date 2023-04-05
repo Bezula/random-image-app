6d4f1a2f1b90b8130e1173937ea4dd6cc644d56a
Maciej <m.kulinski@codelabs.pl>
2023-04-05 19:49:16 +0200

    (050423) random images grid loads next page if user scrolls down

--- src/app/ui/photo-grid/photo-grid.component.ts
+++ src/app/ui/photo-grid/photo-grid.component.ts
@@ -1,5 +1,11 @@
-import { Component, OnDestroy, OnInit } from '@angular/core';
-import { Subscription, tap } from 'rxjs';
+import {
+  Component,
+  ElementRef,
+  OnDestroy,
+  OnInit,
+  ViewChild,
+} from '@angular/core';
+import { filter, map, mergeMap, Subscription, tap } from 'rxjs';
 import { WindowService } from 'src/app/core/services';
 import { Photo, PhotosHttpService } from 'src/app/shared';
 
@@ -9,7 +15,10 @@ import { Photo, PhotosHttpService } from 'src/app/shared';
   styleUrls: ['./photo-grid.component.scss'],
 })
 export class PhotoGridComponent implements OnInit, OnDestroy {
+  @ViewChild('list') list: ElementRef | undefined;
   photos: Photo[] = [];
+  pageNum: number = 1;
+  private readonly query = 'Computer examples';
 
   private readonly subscriptions$ = new Subscription();
 
@@ -21,14 +30,43 @@ export class PhotoGridComponent implements OnInit, OnDestroy {
   ngOnInit(): void {
     this.subscriptions$.add(
       this.photosHttpService
-        .searchPhotos('Computer examples')
-        .pipe(tap((res) => (this.photos = res.photos)))
+        .searchPhotos(this.query)
+        .pipe(
+          tap(({ photos, page }) => {
+            this.photos = photos;
+            this.pageNum = page;
+          })
+        )
         .subscribe()
     );
 
     this.subscriptions$.add(
       this.windowService.scroll$
-        .pipe(tap(() => console.log('scroll')))
+        .pipe(
+          filter(() => {
+            const listElement = this.list?.nativeElement as HTMLElement;
+            const scrollTop = window.scrollY;
+            const viewportHeight = window.innerHeight;
+            /**
+             * Height in pixels
+             */
+            const otherContentHeight = 123;
+
+            return (
+              scrollTop + viewportHeight - otherContentHeight >=
+              listElement.offsetHeight
+            );
+          }),
+          mergeMap(() =>
+            this.photosHttpService.searchPhotos(this.query, this.pageNum).pipe(
+              tap(({ photos, page }) => {
+                //FIXME: To optymize
+                this.photos = [...this.photos, ...photos];
+                this.pageNum = page;
+              })
+            )
+          )
+        )
         .subscribe()
     );
   }
