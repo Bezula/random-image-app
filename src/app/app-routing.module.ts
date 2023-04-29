ac8f87814140f34254c5bb42bf656764073bcd99
Maciej <m.kulinski@codelabs.pl>
2023-04-29 11:35:41 +0200

    (290423) primary and secondary buttons is implemented, test path route is created for dev testing purposes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LayoutComponent } from './ui';
 import { RandomComponent } from './ui/random/random.component';
+import { TestComponent } from './shared/test/test.component';
 
 const routes: Routes = [
   {
@@ -11,6 +12,10 @@ const routes: Routes = [
       {
         path: 'random',
         component: RandomComponent,
+      },
+      {
+        path: 'test',
+        component: TestComponent,
       },
     ],
   },
